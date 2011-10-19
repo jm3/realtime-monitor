1a9de09effb36bf41983fba0e3b71e394a53ecfd
John Manoogian III <jm3@jm3.net>
2011-10-18 23:26:20 -0700

    ghetto version; auto-clear old

--- public/javascripts/monitor.js
+++ public/javascripts/monitor.js
@@ -2,15 +2,24 @@
 
 var source = new EventSource('stream');
 var max_length = 10000;
-var uid;
+var max_faces = 200; 
+var uid, faces = 0;
+
 source.onmessage = function (event) {
   if( $("#log").html().length > max_length )
     $("#log").html( $("#log").html().substring(0, max_length));
 
   $("#log").html( 
-    event.data + "\n\n" + $("#log").html() );
-    event.data.match( /&(user_id|hb)=(\d+)/ );
-    uid = RegExp.$2;
-    $("body").append( "<img src='http://img.tweetimag.es/i/" + uid + "_n' title='" + uid + "' width='73' height='73' />" );
+    event.data + "\n" + $("#log").html() );
+
+  if( ++faces > max_faces ) { 
+    console.log( "hit!");
+    $("#faces").html( "" );
+    faces = 0;
+  }
+
+  event.data.match( /&(user_id|hb)=(\d+)/ );
+  uid = RegExp.$2;
+  $("#faces").append( "<img src='http://img.tweetimag.es/i/" + uid + "_n' title='" + uid + "' width='73' height='73' />" );
 };
 
