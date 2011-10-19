b508424c1324765ae3b3e10a29892bca64830299
John Manoogian III <jm3@jm3.net>
2011-10-18 23:06:23 -0700

    icon streaming.

--- public/javascripts/monitor.js
+++ public/javascripts/monitor.js
@@ -2,11 +2,15 @@
 
 var source = new EventSource('stream');
 var max_length = 10000;
+var uid;
 source.onmessage = function (event) {
   if( $("#log").html().length > max_length )
     $("#log").html( $("#log").html().substring(0, max_length));
 
   $("#log").html( 
     event.data + "\n\n" + $("#log").html() );
+    event.data.match( /&(user_id|hb)=(\d+)/ );
+    uid = RegExp.$2;
+    $("body").append( "<img src='http://img.tweetimag.es/i/" + uid + "_n' title='" + uid + "' width='73' height='73' />" );
 };
 
